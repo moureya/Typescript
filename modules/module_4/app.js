993befdcb49554e7f763c5741944a408f60a42b5
Anthony <moureya01@gmail.com>
2018-06-18 20:57:26 -0400

    Completed Section 4

--- modules/module_4/app.js
+++ modules/module_4/app.js
@@ -27,3 +27,47 @@ var multiplyNumbers = function (number1, number2) {
     return number1 * number2;
 };
 console.log(multiplyNumbers(10, 4));
+var greet = function () {
+    console.log('Hello!');
+};
+greet();
+var greetFriend = function (friend) { return console.log(friend); };
+greetFriend('Manu');
+//Default Parameters
+console.log('DEFAULT PARAMETERS');
+var countdown = function (start) {
+    if (start === void 0) { start = 10; }
+    console.log(start);
+    while (start > 0) {
+        start--;
+        console.log(start);
+    }
+    console.log('Done!', start);
+};
+countdown();
+//Rest & Spread
+console.log('REST & SPREAD');
+var numbers = [1, 10, 99, -5];
+console.log(Math.max(33, 99, 10, -3));
+console.log(Math.max.apply(Math, numbers));
+function makeArray(name) {
+    var args = [];
+    for (var _i = 1; _i < arguments.length; _i++) {
+        args[_i - 1] = arguments[_i];
+    }
+    return args;
+}
+console.log(makeArray('Max', 1, 2, 6));
+// Descructuring
+console.log('DESCTRUCTURING');
+var myHobbies = ['Cooking', 'Sports'];
+var hobby1 = myHobbies[0], hobby2 = myHobbies[1];
+console.log(hobby1, hobby2);
+var userData1 = { userName: 'Anthony', age: 28 };
+var userName = userData1.userName, age = userData1.age;
+console.log(userName, age);
+//Template Literals
+console.log('TEMPLATE LITERALS');
+var firstName = 'Anthony';
+var hello = "Hello, my name is " + firstName;
+console.log(hello);
--- modules/module_4/app.ts
+++ modules/module_4/app.ts
@@ -32,3 +32,56 @@ const multiplyNumbers = (number1: number, number2: number) => {
     return number1 * number2;
 };
 console.log(multiplyNumbers(10, 4));
+
+const greet1 = () => {
+    console.log('Hello!');
+};
+greet1();
+
+const greetFriend = (friend: string) => console.log(friend);
+greetFriend('Manu');
+
+//Default Parameters
+console.log('DEFAULT PARAMETERS');
+
+const countdown = (start: number = 10): void => {
+    console.log(start);
+    while (start > 0) {
+        start--;
+        console.log(start);
+    }
+    console.log('Done!', start);
+};
+countdown();
+
+//Rest & Spread
+console.log('REST & SPREAD');
+
+const numbers1 = [1, 10, 99, -5];
+console.log(Math.max(33, 99, 10, -3));
+console.log(Math.max(...numbers1));
+
+function makeArray(name: string, ...args: number[]) {
+    return args;
+}
+console.log(makeArray('Max', 1, 2, 6));
+
+// Descructuring
+console.log('DESCTRUCTURING');
+
+const myHobbies = ['Cooking', 'Sports'];
+const [hobby1, hobby2] = myHobbies;
+console.log(hobby1, hobby2);
+
+const userData1 = { userName: 'Anthony', age: 28 };
+const { userName, age } = userData1;
+console.log(userName, age);
+
+//Template Literals
+console.log('TEMPLATE LITERALS');
+
+const firstName1 = 'Anthony';
+const hello = `Hello, my name is ${firstName1}`;
+console.log(hello);
+
+
